0cd7e30f0a485400b9d5a6c8d00f8f19d2dc70c6
erangeljr <hacker@eddierangel.com>
2013-05-25 13:54:43 -0700

    Adding Private Variables

--- src/Server.java
+++ src/Server.java
@@ -1,4 +1,17 @@
+import java.net.*;
+import java.io.*;
+
 
 public class Server {
+	
+	//Output Stream to Client
+	private ObjectOutputStream output;
+	//Input Stream from Client
+	private ObjectInputStream input;
+	//ServerSocket
+	private ServerSocket server;
+	//Connection to Client
+	private Socket connection;
+	private int counter = 1;
 
 }
