19d365261f5e09d17e79697eed67b6ba4dd8e4db
erangeljr <hacker@eddierangel.com>
2013-05-25 14:00:41 -0700

    Adding Server and ServerSocket Components
    To Server Class

--- src/Server.java
+++ src/Server.java
@@ -1,6 +1,30 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 
 public class Server {
 	
-	
+	static Socket socket = null;
+	static ServerSocket serverSocket = null; 
+
+	public static void main(String argv[]){
+
+		try{
+
+			serverSocket = new ServerSocket(9999);
+		}
+		catch(IOException e){
+			e.printStackTrace();
+			return;
+		}
+			              
+
+//		while(true){
+//			socket = serverSocket.accept();
+//			(new Thread (ServerAgent(socket))).start();
+//
+//		}	
+	}
 
 }
